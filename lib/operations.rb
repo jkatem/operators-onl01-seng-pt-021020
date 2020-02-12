362f07d9d95145a7567af15eb0c2cb003c50c9d3
Jen Miao <jenmiao@gmail.com>
2020-02-12 17:50:57 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
-
+  #return true if speed is unsafe
+  if speed == unsafe
+    return true
+  else
+    return false
+  #return false if it is safe
 end
 
 
 
 def not_safe?(speed)
-	
+
 end
-	
-
-
